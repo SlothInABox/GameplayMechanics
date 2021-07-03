31ad1600ecbc9fcce0cddec22f874f4af04aec1d
James Goodman <43926484+SlothInABox@users.noreply.github.com>
2021-07-03 17:47:28 +0100

    Completed challenge 4
    When the player collides with an enemy they now move away from the player
    A new enemy wave will spawn when all enemies are removed
    The powerup now disappears after a duration
    More enemies now spawn in higher waves
    Enemies now move towards the player goals
    When the spacebar is pressed, the player gains a speed boost and a particle effect plays
    Enemies speed increases with each wave

---
+++ Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    public float speed;
+    private float speedIncrement = 10;
+    private Rigidbody enemyRb;
+    private GameObject playerGoal;
+
+    public SpawnManagerX spawnManagerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        playerGoal = GameObject.Find("Goals/Player Goal");
+        spawnManagerScript = GameObject.Find("Spawn Manager").GetComponent<SpawnManagerX>();
+
+        speed += spawnManagerScript.waveCount * speedIncrement;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set enemy direction towards player goal and move there
+        Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed * Time.deltaTime);
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal")
+        {
+            Destroy(gameObject);
+        } 
+        else if (other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+}
